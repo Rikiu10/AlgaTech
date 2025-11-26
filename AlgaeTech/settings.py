7239bf344e476db9613d76115f5f50967818ead2
Rikiu_10 <ricardoporriga0@gmail.com>
2025-11-26 20:54:25 -0300

    mysql
    ya configurado, recuerden que deben crear una base de datos con el user root, ademas del schema con el mismo nombre que esta en settings que es : alga_tech_db

--- AlgaeTech/settings.py
+++ AlgaeTech/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = 'AlgaeTech.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # Usamos el motor de MySQL
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'alga_tech_db',             
+        'USER': 'root',                      
+        'PASSWORD': '123456',   
+        'HOST': '127.0.0.1',                 
+        'PORT': '3306',                      
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
+
 
 
 # Password validation
